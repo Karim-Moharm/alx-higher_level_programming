f1c499ed2761be5d399a24df67cd3ec7ef8de687
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-06 13:24:47 -0700

    lower case char except e and q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z')):
+    # ord used to get the ascii value of char
+    if i == ord('q') or i == ord('e'):
+        continue
+    print("{:c}".format(i), end="")
