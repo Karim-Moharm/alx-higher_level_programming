2a52c2e01fed60b2fd896bf4e328848ad524f664
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-06 14:21:42 -0700

    print hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range (98):
-    print(f"{i} = 0x{i}" if i < 9 else f"{i} = 0x{str ((int(i) / 16) % 16) + str (int(i) % 16)}")
+for i in range(99):
+    print(f"{i} = {hex(i)}")
