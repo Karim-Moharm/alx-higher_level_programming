53635dc1416b577781f9d6b0420436a8276d78a2
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-06 13:25:38 -0700

    fix last digit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')):
+for i in range(ord('a'), ord('z') + 1):
     # ord used to get the ascii value of char
     if i == ord('q') or i == ord('e'):
         continue
