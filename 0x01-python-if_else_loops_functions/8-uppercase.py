614b52ebe9cf78cd17b8e5ed30726aeb838d20c9
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-07 10:02:53 -0700

    fix upper char

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if (str[i] >= 'a' and str[i] <= 'z'):
-            str[i] = chr(ord(str[i]) - 32)
-        print("{}".format(str[i]), end="")
-
-    print() 
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            print("{}".format(chr(ord(i) - 32)), end="")
+    print()
