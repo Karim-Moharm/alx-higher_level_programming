f785ffa19bb6c367eba6be65515a42810f3b91f5
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-07 10:29:53 -0700

    fix first char

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a'), -1):
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2:       #if ascii for char is odd, convert it to upper
         i = chr(i - 32)
     else:
