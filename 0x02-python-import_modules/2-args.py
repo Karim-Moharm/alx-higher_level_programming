0c52cd48965f16d5ab974cd2c1c34787194106a2
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-11 09:18:04 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print(f"{argc} arguments.")
+    elif argc == 1:
+        print(f"{argc} argument:")
+        print(f"{argc}: {sys.argv[argc]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(argc):
+            print(f"{i + 1}: {sys.argv[i + 1]}")
+            # print("{:d}: {:s}".format(i, sys.argv[i]))
