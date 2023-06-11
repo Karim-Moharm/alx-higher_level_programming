87d27b36e531657ca5ba466d047883e87c2ef1c9
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-11 09:33:10 -0700

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def arg_addition(av):
+    add = 0
+    for i in range (len(av) - 1):
+        add += int(av[i + 1])
+    print(f"{add}")
+
+
+if __name__ == "__main__":
+    import sys
+    arg_addition(sys.argv)
