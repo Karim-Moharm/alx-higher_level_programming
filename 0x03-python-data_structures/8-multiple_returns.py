b687553c7e95abb0016a0df38c085e4278f1a506
karim Moharm <112572571+Karim-Moharm@users.noreply.github.com>
2023-06-12 19:54:24 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    """
+    a function that returns a tuple with the length of a string and its first character.
+    """
     if sentence:
         return len(sentence), sentence[0]
     return len(sentence), None
