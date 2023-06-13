f1c07ec200dba70087c73748e87bc0a0b2e2aed9
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-13 16:21:13 -0700

    true or false

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_lst = list()
+    for i in my_list:
+        if i % 2:
+            new_lst.append(False) 
+        else:
+            new_lst.append(True)
+    return new_lst
