d63f4f32d749ecb7b9e5dae9553ec3e9a9b134ce
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-13 15:38:28 -0700

    trying after update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        if tuple_a[0] == None:
+        if len(tuple_a) == 0:
             list_a = list(tuple_a)
-            list_a[0] = 0
+            list_a = [0, 0]
             tuple_a = tuple(list_a)
-        else:
+        elif len(tupl_a) == 1:
             list_a = list(tuple_a)
             list_a[1] = 0
             tuple_a = tuple(list_a)
 
     if len(tuple_b) < 2:
-        if tuple_b[0] == None:
+        if len(tuple_b) == 0:
             list_b = list(tuple_b)
-            list_b[0] = 0
+            list_b = [0, 0]
             tuple_b = tuple(list_b)
-        else:
+        elif len(tuple_b) == 1:
             list_b = list(tuple_b)
             list_b[1] = 0
             tuple_b = tuple(list_b)
