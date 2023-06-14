7c1a239eee689ba2fad6d5a5d37873aaf1be36b6
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-14 14:23:06 -0700

    fixing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_matrix.append(list(map(lambda elem: elem**2 ,row)))
+        new_matrix.append(list(map(lambda elem: elem**2, row)))
     return new_matrix
