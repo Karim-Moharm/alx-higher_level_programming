e7b2f7048e24f5c0719eab2ad71214103291a4c5
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-14 14:21:20 -0700

    fix square of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix[:]
-    for row in new_matrix:
-        new_matrix = list((map(lambda elem: elem**2 , row)))
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(list(map(lambda elem: elem**2 , row)))
     return new_matrix
