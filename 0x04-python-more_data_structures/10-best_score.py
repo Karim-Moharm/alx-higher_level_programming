bcc03de3b4bab7eee836791f925046d4c253dd39
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-14 17:32:05 -0700

    and the winner is

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    first_key = list(a_dictionary.keys())[0]
+    first_value = a_dictionary[first_key]
+    max_key = ""
+    for key, value in a_dictionary.items():
+        if value > first_value:
+            first_value = value
+            max_key = key
+    return max_key
