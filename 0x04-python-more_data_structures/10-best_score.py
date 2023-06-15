8a88f212969b94be8ff2e75d8d0babf542ef68f0
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-14 17:39:57 -0700

    max score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
+    
     first_key = list(a_dictionary.keys())[0]
     first_value = a_dictionary[first_key]
-    max_key = ""
+    max_key = str()
+
     for key, value in a_dictionary.items():
         if value > first_value:
             first_value = value
