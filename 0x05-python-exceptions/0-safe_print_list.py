4477d04374adc96d3d99a0412bc73ce8faf5d908
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-26 09:48:47 -0700

    safe version for printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """
+    a function that prints x elements of a list.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+    except:
+        print()
+    return count
