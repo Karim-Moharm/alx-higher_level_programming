8ef47c1d7b7a9b2377f94041e2dccc5ee97140de
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-26 12:30:48 -0700

    fix float division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,11 +6,9 @@ def safe_print_division(a, b):
     """
     c = None
     try:
-        c = a // b
-
+        c = a / b
     except ZeroDivisionError:
         pass
-
     finally:
         print("Inside result: {}".format(c))
         return c
