9084c38692ee005e33be1288e01baf3d6d141da7
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-26 17:15:06 -0700

    test error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
         return True
     except (ValueError, TypeError):
         raise Exception ("Exception: Unknown format code 'd'\
-                for object of type 'str'", file = sys.stderr)
+                for object of type 'str'", file=sys.stderr)
         return False
 
