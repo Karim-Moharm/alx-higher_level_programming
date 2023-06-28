8af6a954a1a7de9636d29d104a65f24076ee7c05
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-27 18:14:19 -0700

    addign size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """a class Square that defines a square
+
+    Attributes:
+        __size (int): size of the square
+    
+    """
+
+    def __init__(self, size):
+        self.__size = size
+
