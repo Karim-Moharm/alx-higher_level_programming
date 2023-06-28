6d9a4ea550e7842c97fb919bdc3aca0f6b861f52
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-27 18:17:56 -0700

    doc for the func

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -11,5 +11,11 @@ class Square:
     """
 
     def __init__(self, size):
+        """the __init__ special method
+
+        Args:
+            self: the instance of class
+            size: size of square
+        """
         self.__size = size
 
