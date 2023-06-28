efabda41a9f282df53ba809fe44a9a107ad49647
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-27 18:20:22 -0700

    fix doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,8 +6,7 @@ class Square:
     """a class Square that defines a square
 
     Attributes:
-        __size (int): size of the square
-    
+        size (int): size of the square
     """
 
     def __init__(self, size):
