43163dce1a849550fdc4508b7288957b6305da1e
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-29 10:33:11 -0700

    access and update private attr

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,5 +3,27 @@
 
 
 class Square:
+    """Square class that defines a square
+
+    Attributes:
+        size (int): private attribute
+    """
+
     def __init__(self, size=0):
-        pass
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
