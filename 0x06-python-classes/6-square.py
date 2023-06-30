8f3be88707d488475e3022b94ceef8591792d905
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-29 18:18:26 -0700

    update square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,10 +43,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if value[0] < 0 or value[1] < 0:
+        if (type(value) is not tuple or
+                value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-           self.__position = value
+            self.__position = value
 
     def area(self):
         """Method to Calculate the area of the square
