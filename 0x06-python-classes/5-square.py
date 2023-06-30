4ea932d77e886f68d61dc0ce53a55f9d083096ea
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-29 19:23:36 -0700

    docstring for last method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,9 +45,15 @@ class Square:
             self.__size = value
 
     def area(self):
+        """Calculates the Area of square
+
+        Returns:
+            area of square
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """prints in stdout the square with the character"""
         if self.__size == 0:
             print()
         else:
