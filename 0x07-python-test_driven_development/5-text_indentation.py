66bc487f801e58151df951df0f4eeb31ec9d5151
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-02 20:07:02 -0700

    fix task5 doc

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,8 +5,9 @@ The module have a function that separated and print it into new lines
 Author: Karim Moharm
 """
 
+
 def text_indentation(text):
-    """prints a text with 2 new lines after each of these characters: ., ? and :
+    """prints a text with 2 new lines after each of these chars: ., ? and :
 
     Args:
         test: text to be printed
@@ -16,13 +17,13 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
-    char = ['.', '?',':']
+
+    char = ['.', '?', ':']
 
     string = text.replace(char[0], char[0] + "\n\n")
     string = string.replace(char[1], char[1] + "\n\n")
     string = string.replace(char[2], char[2] + "\n\n")
-    
+
     for _ in range(len(text)):
         string = string.replace("\n ", "\n")
 
