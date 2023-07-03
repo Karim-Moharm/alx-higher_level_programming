928e0fbc16d6d13be3c1837870d5f6aed639f099
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-02 18:55:10 -0700

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The module has one function that prints a name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """a function that prints a full name
+
+    Args:
+        first_name (str): first name to print
+        last_name (str): last name to print
+
+    Raises:
+        TypeError: if the names are not string
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
