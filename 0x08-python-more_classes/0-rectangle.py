69ac783cd85a97b952d19635277b59d106adcc87
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-03 17:23:04 -0700

    empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+Module that Creates an empty class
+
+Author: Karim Moharm
+"""
+
+
+class Rectangle:
+    """Empty class called Rectangle
+
+    Attributes:
+        empty
+    """
+    pass
