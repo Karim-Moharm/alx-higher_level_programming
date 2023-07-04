2ceeb97c42ee2990aa1199d2b754872e52c9b80a
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-03 20:07:23 -0700

    finishing matrix_division test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -5,5 +5,13 @@ matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
+
+matrix2 = [
+        [11, 2, 4],
+        [78, 45, True]
+    ]
 print(matrix_divided(matrix, 3))
 print(matrix)
+
+print(matrix_divided(matrix2, 3))
+print(matrix2)
