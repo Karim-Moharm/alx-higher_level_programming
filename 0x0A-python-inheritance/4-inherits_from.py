8c7d8cac768d2ad7884cb758301b1885788f307f
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-10 19:54:45 -0700

    test indirect method

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,4 +17,4 @@ def inherits_from(obj, a_class):
         True if the object instance of a class that inherited
         (directly or indirectly) from the specified class; otherwise False
     """
-    return issubclass(type(obj), a_class) or isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) or type(obj) is not a_class
