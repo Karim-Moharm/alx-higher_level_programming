6aacb972f32c4bbffe9b691aaa0b5098bc7d59ea
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-10 20:24:15 -0700

    check eq and ne

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,6 +4,20 @@ MyInt Class
 """
 
 
-class MyINnt(int):
+class MyInt(int):
     """MyInt syb-class"""
-    pass
+    
+    def __init__(self, num):
+        self.num = num
+
+    def __eq__(self, other):
+        if isinstance(other, MyInt):
+            if self.num == other.num:
+                return False
+        return True
+
+    def __ne__(self, other):
+        if isinstance(other, MyInt):
+            if self.num != self.num:
+                return False
+        return True
