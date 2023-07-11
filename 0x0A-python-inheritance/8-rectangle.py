573af3473d02d3fe2c5e02ddd00c02da25c745f5
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-10 19:08:51 -0700

    rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rectangle class Module
+"""
+
+form 7-base_geometry import BaseGeometry
+class Rectangle(BaseGeometry):
+    """
+    Rectangle subclass that inherit from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+
