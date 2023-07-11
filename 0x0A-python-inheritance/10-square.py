36e951454164b39db49e62dff78bfc87330c165a
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-10 19:33:13 -0700

    adding docstring

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,6 +10,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
+        """The __init__ special method
+        Args:
+            size (int): size of square
+        """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
