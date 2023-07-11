2e1760b2623da25a0653232f5dea9b992f18862a
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-11 16:29:18 -0700

    reading a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module having a function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """reads a text file
+
+    Args:
+        filename: the file to be readed
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
