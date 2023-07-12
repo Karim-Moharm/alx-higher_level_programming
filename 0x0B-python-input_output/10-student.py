e5407ac64a1a94b90389ec72b6f68074b184606d
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-11 19:22:25 -0700

    filter to json

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,5 +11,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
+        is_string = True
+
+        if type(attrs) == list:
+            for elem in attrs:
+                if type(elem) != str:
+                    is_string = False
+
+        if type(attrs) == list and is_string:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
         return self.__dict__
