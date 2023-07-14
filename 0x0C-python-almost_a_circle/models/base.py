ba7fccc708692b3996a98520249ec538f98daeda
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-14 16:00:40 -0700

    update base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,13 +6,18 @@
 class Base:
     """
     The Base Class of the project
+
+    Attrs:
+         __nb_objects: private class attribute
     """
     __nb_objects = 0
     def __init__(self, id=None):
         """init special method that works as constructor
+        Attrs:
+            id (int): integer attribute for id
         """
         if id in not None:
             self.id = id
         else:
-            __nb_objects += 1
-            id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
