cf3f01861d8a1de30489c18b759e72a0cf46b664
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-14 16:50:36 -0700

    rectangle sub class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+"""Module for rectangle sub-class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init magic method that works as a constructor
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x 
+        self.__y = y
+        super().__init__(id)
