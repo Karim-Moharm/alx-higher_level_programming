7d7f198fc323aed375a63cef675e2bd79c491027
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-14 16:01:56 -0700

    fix style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,6 +11,7 @@ class Base:
          __nb_objects: private class attribute
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """init special method that works as constructor
         Attrs:
