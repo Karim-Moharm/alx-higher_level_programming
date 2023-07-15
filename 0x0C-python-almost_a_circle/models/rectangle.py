885a1863f12a603dd184ef6415ddbadd5fd14975
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-15 10:50:42 -0700

    fix args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,9 +96,13 @@ class Rectangle(Base):
                 self.height)
     
     def update(self, *args):
-        for arg in args:
-            self.id = arg
-            self.width = arg
-            self.height = arg
-            self.x = arg
-            self.y = arg
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
