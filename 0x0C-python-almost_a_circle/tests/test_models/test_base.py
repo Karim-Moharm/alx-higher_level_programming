8cac44c029afc4d1b6555a3725c55461980eda06
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-17 06:08:45 -0700

    test id

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""Test base module
+"""Module to test Base class
 """
+import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
-import unittest
 
 
 class TestBaseClass(unittest.TestCase):
-    """test cases of Base class
-    """
-    pass
+    """Tests cases for Base Class
+    """ 
+    ''' Issue....
+    def test_class_attribute(self):
+        """test if nb_object is initialized with zero
+        """
+        self.assertEqual(getattr(Base, 'Base.__nb_object'), 0)
+    '''
+
+    def test_id_attr(self):
+        """test id attribute of Base class
+        """
+        b1 = Base(10)
+        self.assertEqual(b1.id, 10)
