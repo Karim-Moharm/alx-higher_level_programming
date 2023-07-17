11a11e3661903e25f96fd108e1cad6884b4599b3
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-17 04:08:46 -0700

    starting base class test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Test base module
+"""
+from model.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import unittest
+
+
+class TestBaseClass(unittest.TestCase):
+    """test cases of Base class
+    """
+    pass
