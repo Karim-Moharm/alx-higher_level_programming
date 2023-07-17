b86d1eeb3e715fc4201ab9e8e7b7903e8932af36
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-17 16:17:48 -0700

    some test cases of rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -24,9 +24,75 @@ class TestRectangleModule(unittest.TestCase):
     def test_only_two_attrs(self):
         """passing width and height attrivutes only
         """
+        Base._Base__nb_objects = 0  # reset ip value to 0
         r2 = Rectangle(4, 1)
         self.assertEqual(r2.width, 4)
         self.assertEqual(r2.height, 1)
         self.assertEqual(r2.x, 0)
         self.assertEqual(r2.y, 0)
         self.assertEqual(r2.id, 1)
+
+    def test_one_attr(self):
+        """passing only one attribute
+        """
+        with self.assertRaises(TypeError):
+            r3 = Rectangle(1)
+
+    def test_no_attr(self):
+        """test in case no attribute are passed
+        """
+        with self.assertRaises(TypeError):
+            r4 = Rectangle()
+
+    def test_wrong_types(self):
+        """test with wrong attributes types
+        """
+        with self.assertRaises(TypeError):
+            rect = Rectangle(0.855, 4.4)
+
+        with self.assertRaises(TypeError):
+            rect = Rectangle('w', 'h')
+        
+        with self.assertRaises(TypeError):
+            rect = Rectangle(True, False)
+        
+        with self.assertRaises(TypeError):
+            rect = Rectangle((1, 2), (3, 4))
+        
+        with self.assertRaises(TypeError):
+            rect = Rectangle([1, 2], [3, 4])
+        
+        with self.assertRaises(TypeError):
+            rect = Rectangle({1: 3, 2: 4}, 5)
+
+    def test_get_width(self):
+        """test case of trying to access the private 
+        attr width
+        """
+        rec = Rectangle(3, 5)
+        with self.assertRaises(AttributeError):
+            print(rec.__width)
+
+    def test_get_height(self):
+        """test case of trying to access the private 
+        attr height
+        """
+        rec = Rectangle(3, 5)
+        with self.assertRaises(AttributeError):
+            print(rec.__height)
+
+    def test_get_x(self):
+        """test case of trying to access the private 
+        attr x
+        """
+        rec = Rectangle(3, 5)
+        with self.assertRaises(AttributeError):
+            print(rec.__x)
+
+    def test_get_y(self):
+        """test case of trying to access the private 
+        attr y
+        """
+        rec = Rectangle(3, 5)
+        with self.assertRaises(AttributeError):
+            print(rec.__y)
