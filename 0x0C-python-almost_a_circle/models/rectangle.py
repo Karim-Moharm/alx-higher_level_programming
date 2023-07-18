10c4f82c0abfc0761575df8d1ada7fd7f3b6f126
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-17 21:33:20 -0700

    fix rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """init magic method that works as a constructor
         """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
