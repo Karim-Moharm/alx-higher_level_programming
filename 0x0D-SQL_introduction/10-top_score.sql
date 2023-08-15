513c9a1a99dbef79fd76c3190ad8b83c34c835a7
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-15 16:32:52 -0700

    select with specific order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- Records ordered by score (top first) 
+
+SELECT second_table.score, second_table.name 
+FROM `second_table`
+ORDER BY score DESC
+
