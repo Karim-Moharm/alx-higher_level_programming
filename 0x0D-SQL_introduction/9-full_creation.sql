e72edefcc8bb70c2ce779933e7440ea0fce826c4
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-15 16:06:20 -0700

    adding second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- create table called `second_table`
+-- add some info to it
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT PRIMARY KEY,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table
+VALUES  (1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'Georage', 8)
