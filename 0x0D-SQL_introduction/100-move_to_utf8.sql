517919ecb5b30747cf79646172cfa7cc56e7387a
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-15 18:36:42 -0700

    fix convert database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,7 @@
 -- script that converts hbtn_0c_0 database to UTF8
 
+USE hbtn_0c_0;
+
 -- converting database
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
@@ -8,4 +10,10 @@ COLLATE utf8mb4_unicode_ci;
 -- converting the table
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-CALLOT utf8mb4_unicode_ci;
+COLLATE utf8mb4_unicode_ci;
+
+-- convert the Field name
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
