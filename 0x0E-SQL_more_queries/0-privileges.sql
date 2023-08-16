02a60148027dc7c3e433cdcbb6b093d78f40d34b
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-16 15:59:40 -0700

    update show privilege for user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- list all privileges for users: user_0d_1 and user_0d_2 on the server
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SELECT user,host FROM mysql.user;
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
