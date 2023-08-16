1c555ce2dfa124a9c927a413a4f0f9a161f52d84
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-16 16:57:18 -0700

    create table with primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.states` (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	CONSTRAINT `state_pk`
+--	CONSTRAINT `state_pk`
 	PRIMARY KEY (`id`)
 );
