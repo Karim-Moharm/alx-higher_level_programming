8c7b526162484342fbb13bf7cf772ce6bd815b30
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-15 18:38:04 -0700

    fix convert database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,11 +2,6 @@
 
 USE hbtn_0c_0;
 
--- converting database
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
 -- converting the table
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
