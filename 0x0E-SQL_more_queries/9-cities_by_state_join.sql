c23b5342fd4d1211122fada1d808452f865566f3
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-16 18:16:35 -0700

    update inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 --  lists all cities contained in the database hbtn_0d_usa
 
 SELECT c.id, c.name, st.name
-FROM cities AS c INNER JOIN states AS st
+FROM cities AS c 
+INNER JOIN states AS st
 ON c.state_id = st.id
 ORDER BY c.id ASC;
