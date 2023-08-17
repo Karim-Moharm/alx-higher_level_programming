514e3f6b65062f320554e3e01c4d444325fadee6
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-16 18:38:34 -0700

    test tv shows and genres

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT tvs.title, tvg.genre_id
 FROM tv_shows AS tvs 
 INNER JOIN tv_show_genres AS tvg
 ON tvs.id = tvg.show_id
-ORDER BY tvs.title, tvg.genere_id
+ORDER BY tvs.title, tvg.genre_id ASC;
