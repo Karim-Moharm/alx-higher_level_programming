bdd7a90b218154ed1fb166de5cad50e2c5f39a23
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-16 18:34:00 -0700

    test tv shows and genres

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+SELECT tvs.title, tvg.genre_id
+FROM tv_shows AS tvs 
+INNER JOIN tv_show_genres AS tvg
+ON tvs.id = tvg.show_id
+ORDER BY tvs.title, tvg.genere_id
