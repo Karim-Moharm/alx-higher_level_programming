2c29b0bb45fb4e03bdb8e410d29213c031b2f6a0
Karim-Moharm <karimmoharm222@gmail.com>
2023-09-12 17:20:10 -0700

    square class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,9 +5,5 @@ const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-<<<<<<< HEAD
-=======
-    this.size = size;
->>>>>>> f382840bf4659d16fa2b00507178a1a5294c3ac9
   }
 };
