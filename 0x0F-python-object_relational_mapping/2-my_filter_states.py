2f407ca62b054d47e51fcff1ed05ef4168a1b694
Karim-Moharm <karimmoharm222@gmail.com>
2023-09-15 15:00:53 -0700

    printing the state in the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""List all states that starts with Upper `N` in the dataBase
+"""
+
+
+import MySQLdb
+import sys
+
+
+def main(av):
+    """main function
+    """
+    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=av[1],
+                                passwd=av[2], db=av[3])
+    cursor = dbconnect.cursor()
+    query = """
+    SELECT id, name
+    FROM states
+    WHERE name=%s
+    ORDER BY id ASC;
+    """
+
+    try:
+        cursor.execute(query, (av[4],))
+        query_rows = cursor.fetchall()
+    except MySQLdb.Error as e:
+        dbconnect.close()
+        sys.exit(1)
+
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    dbconnect.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
