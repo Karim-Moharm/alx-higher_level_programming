470bea8fcc4168fdde9dc3070671b68fb38688e8
Karim-Moharm <karimmoharm222@gmail.com>
2023-09-29 18:00:17 -0700

    print id and name of json file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+
+def main(av):
+    import requests
+
+    if len(av) == 2:
+        letter = av[1]
+    else:
+        letter = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': letter}
+
+    resp = requests.post(url, data=data)
+    json_data = resp.json()
+
+    if json_data != None:
+        print("[<{}>] <{}>".format(json_data["id"], json_data["name"]))
+    else:
+        print('No result')
+
+
