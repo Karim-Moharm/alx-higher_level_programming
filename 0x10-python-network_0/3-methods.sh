1194f4d4da6689888495a401fc76257668bcbb5e
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-02 19:19:32 -0700

    list all options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept for URL
-curl -sI -X OPTIONS "$1" | grep -i "Allow" | cut -d ":" -f 2- | tr -d '\r\n'
+curl -sI -X OPTIONS "$1" | grep -i "Allow" | cut -d " " -f 1
