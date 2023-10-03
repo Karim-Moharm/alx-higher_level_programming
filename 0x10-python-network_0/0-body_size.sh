7e7cb606a0bc4491d748a71be12d14af11362a3d
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-02 18:33:20 -0700

    fix the command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response 
-curl -s $1 | grep 'Content-Length' | echo $2
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
