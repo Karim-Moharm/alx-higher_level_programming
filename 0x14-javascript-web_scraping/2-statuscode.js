2e3daa80cb85d7bdd7d93211f2ea772c01777bcb
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-25 02:31:35 +0300

    test on ubuntu

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+url = process.argv[2];
+if (process.argv.length == 3) {
+    request(url, function (error, response) {
+        if (error) throw error;
+        console.log(`code: ${response.statusCode}`);
+
+    });
+}
