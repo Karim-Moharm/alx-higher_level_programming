51f4181615a6c2c44d12d2b4e763c05b65cde7c5
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-24 18:45:43 -0700

    nmber of characters

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/**
+ * prints the number of movies where
+ * the character “Wedge Antilles” is present
+ */
+
+request = require('request');
+
+url = process.argv[2];
+if (process.argv.length === 3) {
+  request(url, (error, response, body) => {
+	if (error) throw error;
+    const  bodyData = JSON.parse(body).results;
+	const filmsNum = bodyData.length;
+	let count = 0;
+	
+	for (let i = 0; i < filmsNum; i++) {
+      for (let j = 0; j < bodyData[i].characters.length; j++) {
+        if (bodyData[i].characters[j].includes('/18/')) {
+          count++;
+		}
+	  }
+	}
+	  console.log(count);
+  });
+}
