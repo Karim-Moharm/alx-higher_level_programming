e0d293b14ed2971776905ffb36c85a432fb41734
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-01 23:58:11 +0200

    fixing toggle colors

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,14 +1,11 @@
 $(() => {
-    const getHeaderClassColor = $('header').attr('class');
-    // console.log(getHeaderClassColor);
-    $('#toggle_header').click(() => {
-        if (getHeaderClassColor === 'green') {
-            $('header').removeClass();
-            $('header').toggleClass('red');
-        }
-        if (getHeaderClassColor === 'red') {
-            $('header').removeClass();
-            $('header').toggleClass('green');
-        }
-    });
+	const getHeaderClassColor = $('header').attr('class');
+	$('#toggle_header').click(() => {
+		if (getHeaderClassColor === 'green') {
+			$('header').toggleClass('red');
+		}
+		if (getHeaderClassColor === 'red') {
+			$('header').toggleClass('green');
+		}
+	});
 });
