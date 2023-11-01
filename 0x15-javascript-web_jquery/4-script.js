a9bdac3a9d9ecdb7815fc1e428362ab0589b2553
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-02 00:26:12 +0200

    indent using 2 spaces

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,11 +1,11 @@
 $(() => {
-	const getHeaderClassColor = $('header').attr('class');
-	$('#toggle_header').click(() => {
-		if (getHeaderClassColor === 'green') {
-			$('header').toggleClass('red');
-		}
-		if (getHeaderClassColor === 'red') {
-			$('header').toggleClass('green');
-		}
-	});
+  const getHeaderClassColor = $('header').attr('class');
+  $('#toggle_header').click(() => {
+    if (getHeaderClassColor === 'green') {
+      $('header').toggleClass('red');
+    }
+    if (getHeaderClassColor === 'red') {
+      $('header').toggleClass('green');
+    }
+  });
 });
