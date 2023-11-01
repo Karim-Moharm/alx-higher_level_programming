79997f4711890fb22508b6d942cc51a97def5459
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-02 01:31:24 +0200

    making the script work if imported for head tag

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,15 +1,17 @@
-$(() => {
-  $.ajax({
-    type: 'Get',
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    data: {
-      format: 'json'
-    },
-    error: () => {
-      console.error('error while fetching the data');
-    },
-    success: (data) => {
-      $('#hello').html(data.hello);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  $(() => {
+    $.ajax({
+      type: 'Get',
+      url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+      data: {
+        format: 'json'
+      },
+      error: () => {
+        console.error('error while fetching the data');
+      },
+      success: (data) => {
+        $('#hello').html(data.hello);
+      }
+    });
   });
 });
