805b3849afc1aac777c5ffee010b6c0df556ac99
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-02 01:09:37 +0200

    say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,15 @@
+$(() => {
+  $.ajax({
+    type: 'Get',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    data: {
+      format: 'json'
+    },
+    error: () => {
+      console.error('error while fetching the data');
+    },
+    success: (data) => {
+      $('#hello').append(data.hello);
+    }
+  });
+});
